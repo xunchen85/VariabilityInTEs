cb780d0d99ee553a9fe902e774566b26e6905ee6
Xun Chen <chen.xun.3r@kyoto-u.ac.jp>
2022-04-25 20:52:30 +0900

    Update 14.EMC-TE_motif_analysis.R

--- 14.EMC-TE_motif_analysis.R
+++ 14.EMC-TE_motif_analysis.R
@@ -97,8 +97,6 @@ my_theme_2_noLegend = theme(panel.grid.major = element_blank(),
                             legend.key = element_rect(fill = NA))
 ###########################
 ####### load files
-setwd(dir="/Users/xchen/OneDrive - Kyoto University/Projects_Bourque/EMC_project_2020_12_1/Final_Rscripts_2022_4_20/")
-
 ## Input
 # TE list
 TElist = read.delim("inputs/TElist.sep.27",header=T,sep="")
